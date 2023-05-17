cfe1fb6f99127771f46b86f5414c1454186997ff
Davi <davi.raichu@gmail.com>
2023-05-17 11:01:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const resultado = document.getElementById("resultado");
+
+function userPlay(userChoice) {
+    const computerChoice = getComputerChoice();
+    const gameResult = getGameResult(userChoice, computerChoice);
+    resultado.innerHTML = `Você escolheu: ${userChoice}.
+    <br> Computador escolheu: 
+    ${computerChoice}.<br>${gameResult}`;
+} 
+
+function getComputerChoice(){
+    const choices = ["pedra", "papel", "tesoura"];
+    const randomIndex = Math.floor(Math.random()*3);
+    return choices[randomIndex];
+}
+
+function getGameResult(userChoice, computerChoice){
+    if (userChoice === computerChoice) {
+        return "Empate!";
+    }
+    if (
+        (userChoice === "pedra" && computerChoice === "tesoura") ||
+        (userChoice === "papel" && computerChoice === "pedra") ||
+        (userChoice === "tesoura" && computerChoice === "papel")   
+    ){
+        return "Você ganhou !";
+    }
+        return "Você perdeu !";
+}
